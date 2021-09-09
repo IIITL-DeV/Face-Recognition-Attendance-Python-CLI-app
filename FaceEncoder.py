8babc70013e173f70e56864ed3a2ca63871dca92
GustavRamirez <Gustav2k19@gmail.com>
2021-09-09 18:30:38 +0530

    Created encoding updater function which can update all encodings

--- FaceEncoder.py
+++ FaceEncoder.py
@@ -3,31 +3,35 @@ import FirebaseBucket
 import json
 
 class FaceEncoder:
+    saveDir="KnownFaces/"
+    localJsonDir="CurrentFaceData.json/"
+    encDir="encodings.json"
 
-    def getImg(rollNo,saveDir):
-        try:
-            img = fr.load_image_file(saveDir+"/"+rollNo+".jpeg")
-        except:
-            img = fr.load_image_file(FirebaseBucket().downloadFaceImg(rollNo))
-        return img
-
-    def getEcoding(self,rollNo,saveDir):
-        img = None
-        # img = read_img(known_dir + '/' + file)
-        img_enc = fr.face_encodings(img)[0]
-        f = open('encodings.json',)
-        # returns JSON object as 
-        # a dictionary
-        encodings = json.load(f)
-
-        encodings[rollNo] = img_enc
-
-        
-
-
-
-
-
-
+    def getEcoding(self,rollNo):
+        FirebaseBucket.downloadFaceImg(rollNo)
+        img=fr.load_image_file(self.saveDir+rollNo+".jpg")
+        enc = fr.face_encodings(img)[0]
+        FirebaseBucket.deleteImage(self,rollNo)
+        return enc.tolist()
     
-
+    def encodingUpdater(self):
+        f=open(self.localJsonDir)
+        f2=open(self.encDir)
+        locData=json.load(f)
+        fbData=FirebaseBucket.getChangedData()
+        encData=json.load(f2)
+        f.close()
+        f2.close()
+        
+        for roll,dat in fbData.items():
+            if (roll in locData and int(dat)>int(locData[roll])) or (roll not in locData):
+                enc=self.getEcoding(roll)
+                locData[roll]=int(dat)
+                encData[roll]=enc
+        
+        f=open(self.localJsonDir,'w')
+        f2=open(self.encDir,'w')
+        json.dump(locData,f)
+        json.dump(encData,f2)
+        f.close()
+        f2.close()
